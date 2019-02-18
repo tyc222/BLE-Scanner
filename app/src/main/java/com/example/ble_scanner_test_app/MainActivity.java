3f33508145574c694f27596b1c1f802b6b127d22
Tim <>
2019-02-18 10:55:41 +0800

    Current State

---
+++ app/src/main/java/com/example/ble_scanner_test_app/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.ble_scanner_test_app;
+
+import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import javax.xml.datatype.Duration;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static int REQUEST_ENABLE_BT = 1;
+
+    // Declare bluetooth adapter
+    BluetoothAdapter bluetoothAdapter;
+
+    // Define a string adapter which will handle the data of the listview
+    ArrayAdapter<String> listViewAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // If device does not support BLE then let the User know and exit app
+        if (!this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Your Phone Does Not Support BLE")
+                    .setPositiveButton("Exit App", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+
+        // List of Array Strings which will serve as list items
+        ArrayList<String> listItems = new ArrayList<String>();
+
+        // /set up an adapter for listview
+        listViewAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
+
+        // set the data to our listview
+        ListView list = (ListView) findViewById(R.id.list_view_ble_devlice);
+        list.setAdapter(listViewAdapter);
+
+        // Set up bluetooth adapter and ble services on phone
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensure BLE is enabled
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
+        // Scanning for BLE devices
+        BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
+
+
+        final ScanCallback mScanCallback = new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+                BluetoothDevice device = result.getDevice();
+                listViewAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        };
+
+        final BluetoothLeScanner bluetoothLeScanner = ba.getBluetoothLeScanner();
+        bluetoothLeScanner.startScan(mScanCallback);
+
+        Handler mHandler = new Handler() {
+
+        };
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                bluetoothLeScanner.stopScan(mScanCallback);
+                isScanning = false;
+            }
+        }, 10000);
+
+
+
+
+        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+        // Add the name and address to a ListView
+        listViewAdapter.add(device.getName() + "\n" + device.getAddress());
+        Toast.makeText(getApplicationContext(),"Found device " + device.getName(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void scanBleDevices(View view){
+
+
+                }
+
+
+
+
+
+
+
+
+}}
