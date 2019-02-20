bf76a154eca18bfec9051557bae4fad61dfdf30e
Tim <>
2019-02-20 15:37:25 +0800

    Return a list of address. Stop scanning after 5 seconds. Do not duplicate.

--- app/src/main/java/com/example/ble_scanner_test_app/MainActivity.java
+++ app/src/main/java/com/example/ble_scanner_test_app/MainActivity.java
@@ -7,7 +7,9 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -32,13 +34,17 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.xml.datatype.Duration;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Activity";
 
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
 
@@ -46,16 +52,37 @@ public class MainActivity extends AppCompatActivity {
 
     private final static int REQUEST_ENABLE_LOCATION = 1;
 
+    // Scanning time for the phone
+    private final static int SCAN_PERIOD = 5 * 1000;
+
     // Declare bluetooth adapter
     BluetoothAdapter bluetoothAdapter;
 
     // Define a string adapter which will handle the data of the listview
     ArrayAdapter<String> listViewAdapter;
 
+    // Define a ble Scanner object
+    BluetoothLeScanner bluetoothLeScanner;
+
+    // Holder for scancallback
+    ScanCallback scanCallback;
+
+    // Holder for scan results
+    Map<String, BluetoothDevice> scanResults;
+
+    // Determine if the phone is scanning
+    Boolean scanning = false;
+
+    // Handler to stop phone from scanning forever
+    Handler handler;
+
+
+
     // SetMap for duplicated device scan
     Set<String> unduplicatedDeviceMacAddress;
     // Condition for the scan button
     boolean isScanning = false;
+
 
     @Override
     protected void onStart() {
@@ -146,87 +173,157 @@ public class MainActivity extends AppCompatActivity {
         ListView list = (ListView) findViewById(R.id.list_view_ble_devlice);
         list.setAdapter(listViewAdapter);
 
-        // Set up bluetooth adapter and ble services on phone
+        //Set up bluetooth adapter and ble services on phone
         final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
 
 
+    }
+
+
+
+    private void startScan() {
+
+        List<ScanFilter> filters = new ArrayList<>();
+        ScanSettings settings = new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_BALANCED).build();
+        scanResults = new HashMap<>();
+        scanCallback = new BtleScanCallback(scanResults);
+
+        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+        bluetoothLeScanner.startScan(filters, settings, scanCallback);
+        scanning = true;
+        handler = new Handler();
+        handler.postDelayed(this::stopScan, SCAN_PERIOD);
+    }
+
+    // Use the same ScanCallback to avoid unnecessary calls
+    private void stopScan () {
+        if (scanning && bluetoothAdapter != null && bluetoothAdapter.isEnabled() && bluetoothLeScanner != null) {
+            bluetoothLeScanner.stopScan(scanCallback);
+            scanComplete();
+        }
+        scanCallback = null;
+        scanning = false;
+        handler = null;
+    }
+
+    //Perform action using results
+    private void scanComplete() {
+        if (scanResults.isEmpty()) {
+            return;
+        }
+        for (String deviceAddress : scanResults.keySet()) {
+            Log.d(TAG, "Found device: " + deviceAddress);
+            listViewAdapter.add(deviceAddress);
+        }
 
     }
+
+private class BtleScanCallback extends ScanCallback {
+
+
+    private BtleScanCallback(Map<String, BluetoothDevice> scanResultMap) {
+        scanResults = scanResultMap;
+    }
+
+    @Override
+    public void onScanResult(int callbackType, ScanResult result) {
+        addScanResult(result);
+    }
+
+    @Override
+    public void onBatchScanResults(List<ScanResult> results) {
+        for (ScanResult result: results){
+            addScanResult(result);
+        }
+    }
+
+    @Override
+    public void onScanFailed(int errorCode) {
+        Log.e(TAG, "BLE Scan Failed with code" + errorCode);
+    }
+
+    private void addScanResult (ScanResult result){
+        BluetoothDevice device = result.getDevice();
+        String deviceAddress = device.getAddress();
+        scanResults.put(deviceAddress, device);
+    }
+}
 
 
 
     public void scanBleDevices(View view) {
 
-        // Setup BluetoothLeScanner and scanCallback
-        BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
-        ScanCallback scanCallback = null;
-
-        Button scanButton = findViewById(R.id.button);
-
-        // Scanning for BLE devices
-        if (!isScanning) {
-            scanCallback = new ScanCallback() {
-                @Override
-                public void onScanResult(int callbackType, ScanResult result) {
-                    super.onScanResult(callbackType, result);
-
-                    BluetoothDevice device = result.getDevice();
-                    // Add the name and address to a ListView
-                    int checkBondState = device.getBondState();
-                    String bondState;
-                    switch (checkBondState) {
-                        case (12):
-                            bondState = "Paired";
-                            break;
-                        case (11):
-                            bondState = "Paring";
-                            break;
-                        case (10):
-                            bondState = "Unpaired";
-                            break;
-                        default:
-                            bondState = "Error";
-                            break;
-                    }
-
-                    // Double check if the device is already listed, check for duplication
-                    unduplicatedDeviceMacAddress = new HashSet<>();
-
-                    if (!unduplicatedDeviceMacAddress.contains(result.getDevice().getAddress())) {
-                        listViewAdapter.add(device.getName() + "\n" + device.getAddress() + "\n" + bondState
-                                + "\n" + device.getUuids());
-                        unduplicatedDeviceMacAddress.add(device.getAddress());
-                    }
-
-                }
-
-                @Override
-                public void onBatchScanResults(List<ScanResult> results) {
-                    super.onBatchScanResults(results);
-                }
-
-                @Override
-                public void onScanFailed(int errorCode) {
-                    super.onScanFailed(errorCode);
-                    Log.d("TAG", "Scanning Failed " + errorCode);
-                }
-            };
-
-            // Scanning
-            bluetoothLeScanner.startScan(scanCallback);
-
-            scanButton.setText("Stop Scanning");
-        }
-
-        // Stop scanning to save battery
-        if (isScanning) {
-
-            bluetoothLeScanner.stopScan(scanCallback);
-
-            scanButton.setText("Start Scanning");
-        }
-        isScanning = !isScanning;
+        //test
+        startScan();
+//
+//        // Setup BluetoothLeScanner and scanCallback
+//        BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+//        ScanCallback scanCallback = null;
+//
+//        Button scanButton = findViewById(R.id.button);
+//
+//        // Scanning for BLE devices
+//        if (!isScanning) {
+//            scanCallback = new ScanCallback() {
+//                @Override
+//                public void onScanResult(int callbackType, ScanResult result) {
+//                    super.onScanResult(callbackType, result);
+//
+//                    BluetoothDevice device = result.getDevice();
+//                    // Add the name and address to a ListView
+//                    int checkBondState = device.getBondState();
+//                    String bondState;
+//                    switch (checkBondState) {
+//                        case (12):
+//                            bondState = "Paired";
+//                            break;
+//                        case (11):
+//                            bondState = "Paring";
+//                            break;
+//                        case (10):
+//                            bondState = "Unpaired";
+//                            break;
+//                        default:
+//                            bondState = "Error";
+//                            break;
+//                    }
+//
+//                    // Double check if the device is already listed, check for duplication
+//                    unduplicatedDeviceMacAddress = new HashSet<>();
+//
+//                    if (!unduplicatedDeviceMacAddress.contains(result.getDevice().getAddress())) {
+//                        listViewAdapter.add(device.getName() + "\n" + device.getAddress() + "\n" + bondState
+//                                + "\n" + device.getUuids());
+//                        unduplicatedDeviceMacAddress.add(device.getAddress());
+//                    }
+//
+//                }
+//
+//                @Override
+//                public void onBatchScanResults(List<ScanResult> results) {
+//                    super.onBatchScanResults(results);
+//                }
+//
+//                @Override
+//                public void onScanFailed(int errorCode) {
+//                    super.onScanFailed(errorCode);
+//                    Log.d(TAG, "Scanning Failed " + errorCode);
+//                }
+//            };
+//
+//            // Scanning
+//            bluetoothLeScanner.startScan(scanCallback);
+//
+//            scanButton.setText("Stop Scanning");
+//        }
+//
+//        // Stop scanning to save battery
+//        if (isScanning) {
+//
+//        }
+//        isScanning = !isScanning;
     }
+
 
 }
